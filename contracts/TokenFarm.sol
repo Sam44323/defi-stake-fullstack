fd2ca84f581f2962ed45dbccac3c2e97bdbe54a8
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-18 16:29:37 +0530

    add: added functionality listing docs for token issuer

---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,12 @@
+/**
+This very contract allows you to 
+- stake tokens
+- unstake tokens
+- issueTokens i.e issue token rewards
+- addAllowedTokens which allows for more tokens to be staked on your contract
+- getEthValue which gives us the value in eth for the staked tokens on the platform
+ */
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TokenFarm {}
