ce001cc98da6c62b8604fd4d9aa154a91cecb919
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-18 15:36:08 +0530

    update: updated the initial token supply

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DappToken is ERC20 {
     // wei
     constructor(uint256 initialSupply) ERC20("Dapp Token", "DAPP") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, 1000000000000000000000000);
     }
 }
