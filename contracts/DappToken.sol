7a56b8cc0135f050832f13feaf5520870cb2daa6
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-18 15:34:25 +0530

    update: updated the contract sign for token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OurToken is ERC20 {
+contract DappToken is ERC20 {
     // wei
-    constructor(uint256 initialSupply) ERC20("DappToken", "DTP") {
+    constructor(uint256 initialSupply) ERC20("Dapp Token", "DAPP") {
         _mint(msg.sender, initialSupply);
     }
 }
