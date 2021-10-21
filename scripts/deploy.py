cf5b87338a81b241069551b4f56403377b6ca9c6
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-21 22:06:44 +0530

    update: updated the yaml file and added deployment for contracts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,12 @@
 from brownie import DappToken, TokenFarm
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, get_verify_status
 
 
 def deploy_token_farm_and_dapp_token():
     account = get_account()
+    dapp_token = DappToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        dapp_token.address, {"from": account}, publish_source=get_verify_status())
 
 
 def main():
