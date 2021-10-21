66d0f74b540e16791777af4d75c0c47f51da50a6
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-21 22:11:10 +0530

    add: added logic for sending dapp tokens to token farm contract

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,8 @@
 from brownie import DappToken, TokenFarm
 from scripts.helpful_scripts import get_account, get_verify_status
+from web3 import Web3
+
+KEPT_BALANCE = Web3.toWei(100, "ether")
 
 
 def deploy_token_farm_and_dapp_token():
@@ -7,6 +10,11 @@ def deploy_token_farm_and_dapp_token():
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(
         dapp_token.address, {"from": account}, publish_source=get_verify_status())
+
+    # sending most of the dapp tokens to our token farm contract
+    tx = dapp_token.transfer(
+        token_farm.address, dapp_token.totalSupply() - KEPT_BALANCE, {"from": account})
+    tx.wait(1)
 
 
 def main():
