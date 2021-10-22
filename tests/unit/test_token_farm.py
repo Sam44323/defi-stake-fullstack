6053d020e39e027a86ec8924e341c0614966362d
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-22 23:05:41 +0530

    add: added logic for staking a token

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -22,5 +22,24 @@ def test_set_price_feed_contracts():
             dapp_token.address, price_feed_address, {"from": non_owner_account})
 
 
+def test_stake_tokens(amount_staked):
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    price_feed_address = get_contract(
+        "eth_usd_price_feed"). address
+    token_farm.setPriceFeedContract(
+        dapp_token.address, price_feed_address, {"from": account})
+    # sending some dapp_token to the token_farm contract
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeToken(
+        amount_staked, dapp_token.address, {"from": account})
+    assert(
+        token_farm.stakingBalance(
+            dapp_token.address, account.address) == amount_staked
+    )
+
+
 def test_issue_tokens():
     pass
