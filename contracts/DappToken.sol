35bc5fa0cf775f6e60771af9a0cbc469b0e4175d
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-22 22:14:45 +0530

    update: updated the constructor for dapp

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
     // wei
-    constructor(uint256 initialSupply) ERC20("Dapp Token", "DAPP") {
+    constructor() ERC20("Dapp Token", "DAPP") {
         _mint(msg.sender, 1000000000000000000000000);
     }
 }
