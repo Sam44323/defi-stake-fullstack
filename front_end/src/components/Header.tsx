2411f044dcb20fe20d65166311aaa07e3b17f5f4
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-23 23:37:18 +0530

    update: updated the stylings using material and fixed the connection bug using useDapp

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { useEthers } from "@usedapp/core";
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(4),
+    display: "flex",
+    justifyContent: "flex-end",
+    gap: theme.spacing(1),
+  },
+}));
 
 const Header: React.FC = () => {
+  const classes = useStyles();
   const { account, activateBrowserWallet, deactivate } = useEthers();
+  console.log(account);
   return (
-    <div>
+    <div className={classes.container}>
       {account ? (
-        <button color="primary" onClick={() => deactivate()}>
+        <Button color="primary" onClick={deactivate} variant="contained">
           Disconnect
-        </button>
+        </Button>
       ) : (
-        <button color="primary" onClick={() => activateBrowserWallet}>
+        <Button
+          color="primary"
+          onClick={() => activateBrowserWallet()}
+          variant="contained"
+        >
           Connect
-        </button>
+        </Button>
       )}
     </div>
   );
