4458cee5a4f995aade6e15ab804ca0674230ee87
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-23 22:57:51 +0530

    add: added chain-id for config for useDapp

--- front_end/src/index.tsx
+++ front_end/src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ChainId, DAppProvider } from "@usedapp/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider
+      config={{
+        supportedChains: [ChainId.Kovan, ChainId.Rinkeby],
+      }}
+    >
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
