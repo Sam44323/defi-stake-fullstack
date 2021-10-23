fc3f76ba300043cfa0a65a852c0dd94a35285a9c
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-23 21:30:54 +0530

    update: updated the code for integration testing

--- tests/integration/test_token_farm_integration.py
+++ tests/integration/test_token_farm_integration.py
@@ -10,7 +10,7 @@ def test_stake_and_issue_correct_amounts(amount_staked):
     token_farm, dapp_token = deploy_token_farm_and_dapp_token()
     account = get_account()
     dapp_token.approve(token_farm.address, amount_staked, {"from": account})
-    token_farm.stakeTokens(
+    token_farm.stakeToken(
         amount_staked, dapp_token.address, {"from": account})
     starting_balance = dapp_token.balanceOf(account.address)
     price_feed_contract = get_contract("dai_usd_price_feed")
@@ -21,7 +21,7 @@ def test_stake_and_issue_correct_amounts(amount_staked):
     amount_token_to_issue = (
         price / 10 ** price_feed_contract.decimals()
     ) * amount_staked
-    issue_tx = token_farm.issueTokens({"from": account})
+    issue_tx = token_farm.issueToken({"from": account})
     issue_tx.wait(1)
     assert (
         dapp_token.balanceOf(account.address)
