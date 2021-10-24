b4d4496293fe687365b3cc8640546c931531ac55
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 19:09:23 +0530

    add: added tab list for tokens

--- front_end/src/components/YourWallet.tsx
+++ front_end/src/components/YourWallet.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { Token } from "./Main";
+import { Box, Tab } from "@material-ui/core";
+import { TabList, TabContext, TabPanel } from "@material-ui/lab";
 
 const YourWallet: React.FC<{ supportedTokens: Array<Token> }> = ({
   supportedTokens,
 }) => {
-  return <></>;
+  const [selectedTokenIndex, setSelectedTokenIndex] = React.useState<number>(0);
+
+  return (
+    <Box>
+      <h1>Your Wallet</h1>
+      <Box>
+        <TabContext value={selectedTokenIndex.toString()}>
+          <TabList aria-label="stake form tabs">
+            {supportedTokens.map((token, index) => (
+              <Tab
+                label={token.name}
+                value={index.toString()}
+                key={index}
+                onClick={() => setSelectedTokenIndex(index)}
+              />
+            ))}
+          </TabList>
+        </TabContext>
+      </Box>
+    </Box>
+  );
 };
 
 export default YourWallet;
