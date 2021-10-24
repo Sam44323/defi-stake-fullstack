56bd4091148b79d927be75497ed7bb1cadaec7f7
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 20:11:44 +0530

    add: added logic for formatting balance

--- front_end/src/components/WalletBalance.tsx
+++ front_end/src/components/WalletBalance.tsx
@@ -1,6 +1,7 @@
 import { useEthers, useTokenBalance } from "@usedapp/core";
 import React from "react";
 import { Token } from "./Main";
+import { formatUnits } from "@ethersproject/units";
 
 export interface WalletBalanceProps {
   token: Token;
@@ -10,7 +11,10 @@ const WalletBalance: React.FC<WalletBalanceProps> = ({ token }) => {
   const { image, address, name } = token;
   const { account } = useEthers();
   const tokenBalance = useTokenBalance(address, account);
-  return <></>;
+  const formattedBalance: number = tokenBalance
+    ? parseFloat(formatUnits(tokenBalance, 18))
+    : 0;
+  return <div>{formattedBalance}</div>;
 };
 
 export default WalletBalance;
