c22c7a1e7d074f291fd4ae035a2355f1b42c9a6b
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 14:18:00 +0530

    add: added container section

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { Container } from "@material-ui/core";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <Container maxWidth="md">hi</Container>
     </div>
   );
 };
