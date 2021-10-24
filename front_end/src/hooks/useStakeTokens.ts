aaf92c589b0a0074fed8940f6551f19afdb1fb24
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 21:55:48 +0530

    add: added returning data for state and function for approve

--- front_end/src/hooks/useStakeTokens.ts
+++ front_end/src/hooks/useStakeTokens.ts
@@ -1,9 +1,10 @@
-import { useEthers } from "@usedapp/core";
+import { useEthers, useContractFunction } from "@usedapp/core";
 import TokenFarm from "../chain-info/contracts/TokenFarm.json";
 import ERC20 from "../chain-info/contracts/dependencies/OpenZeppelin/openzeppelin-contracts@4.2.0/ERC20.json";
 import mapConfig from "../chain-info/deployments/map.json";
 import { constants, utils } from "ethers";
 import { Contract } from "@ethersproject/contracts";
+import { useState } from "react";
 
 const useStakeTokens = (tokenAddress: string) => {
   // Token Farm Contract
@@ -18,6 +19,17 @@ const useStakeTokens = (tokenAddress: string) => {
   // Token Contract Section
   const erc20Interface = new utils.Interface(ERC20.abi);
   const tokenContract = new Contract(tokenAddress, erc20Interface);
+  const { send: approveErc20Send, state: approveErc20State } =
+    useContractFunction(tokenContract, "approve", {
+      transactionName: "Approve ERC20 transfer",
+    });
+  const approveTokenTransfer = (amount: string) => {
+    return approveErc20Send(tokenFarmAddress, amount);
+  };
+
+  const [state, setState] = useState(approveErc20State);
+
+  return { approveTokenTransfer, state };
 };
 
 export default useStakeTokens;
