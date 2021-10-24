c188d7ace19d8008cf7695007d0bf7129a60b61a
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 21:00:05 +0530

    update: updated the basic structure for StakeForm

--- front_end/src/components/StakeForm.tsx
+++ front_end/src/components/StakeForm.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { Token } from "./Main";
+import { useEthers, useTokenBalance } from "@usedapp/core";
+import { formatUnits } from "@ethersproject/units";
+import { Button } from "@material-ui/core";
 
-const StakeForm: React.FC = () => {
+const StakeForm: React.FC<{ token: Token }> = ({ token }) => {
+  const { account } = useEthers();
+  const { address, name } = token;
+  const tokenBalance = useTokenBalance(address, account);
+  const formattedBalance: number = tokenBalance
+    ? parseFloat(formatUnits(tokenBalance, 18))
+    : 0;
   return <></>;
 };
 
