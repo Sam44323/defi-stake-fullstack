cd13e8503d135c3a358e0eeeadfd707ea1fd8c03
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 16:23:55 +0530

    fix: fixed the network config error

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useEthers } from "@usedapp/core";
+import helperConfig from "../helper-config.json";
+
+type NetworkIDS = 1 | 4 | 42 | 1337;
 
 const Main: React.FC = () => {
+  const { chainId } = useEthers();
+  const networkName = chainId ? helperConfig[chainId as NetworkIDS] : "";
+  console.log(networkName);
   return <></>;
 };
 
