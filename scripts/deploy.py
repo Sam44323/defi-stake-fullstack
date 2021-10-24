ba8626e724d9a65fdafac9a3fd5a724e97e5315a
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 14:47:42 +0530

    add: added update frontend script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,13 @@
 from brownie import DappToken, TokenFarm
 from scripts.helpful_scripts import get_account, get_contract, get_verify_status
 from web3 import Web3
+import yaml
+import json
 
 KEPT_BALANCE = Web3.toWei(100, "ether")
 
 
-def deploy_token_farm_and_dapp_token():
+def deploy_token_farm_and_dapp_token(frontend_update=False):
     account = get_account()
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(
@@ -28,6 +30,8 @@ def deploy_token_farm_and_dapp_token():
         fau_token: get_contract("eth_usd_price_feed")
     }
     add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+    if frontend_update:
+        update_front_end()
     return token_farm, dapp_token
 
 
@@ -39,6 +43,18 @@ def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
             token.address, dict_of_allowed_tokens[token], {"from": account})
         tx.wait(1)
         print("Add tokens addresses for staking and their respective price-feeds!")
+
+
+def update_front_end():
+    # Sending our yaml config to frontend in JSON
+    with open("brownie-config.yaml", "r") as brownie_config:
+        # loading our yaml to the dict
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+
+        # writing the dict as a JSON object in frontend
+        with open('./front_end/src/brownie-config.json', "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print("Front end updated!")
 
 
 def main():
