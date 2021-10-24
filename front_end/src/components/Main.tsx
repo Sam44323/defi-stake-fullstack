890b8a3289d413224821695fe9726605f417d488
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 16:37:56 +0530

    add: added contract address fetcher from map

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useEthers } from "@usedapp/core";
 import helperConfig from "../helper-config.json";
+import mapConfig from "../chain-info/deployments/map.json";
+import { constants } from "ethers";
 
 type NetworkIDS = 1 | 4 | 42 | 1337;
 
 const Main: React.FC = () => {
   const { chainId } = useEthers();
   const networkName = chainId ? helperConfig[chainId as NetworkIDS] : "";
-  console.log(networkName);
+  /**
+   * getting the contract addresses for Dapp-token and Token-Farm. If chainId is not present then use a constant zero address(0x0000...) from ethers
+   */
+  const dappTokenAddress = chainId
+    ? mapConfig[String(chainId) as "42"].DappToken[0]
+    : constants.AddressZero;
+  const tokenFarmAddress = chainId
+    ? mapConfig[String(chainId) as "42"].TokenFarm[0]
+    : constants.AddressZero;
   return <></>;
 };
 
