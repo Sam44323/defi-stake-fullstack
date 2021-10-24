2f471fda6d3a530502dc679fab6fe5e0cf67eb29
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 23:51:11 +0530

    add: added notification updation on state

--- front_end/src/components/StakeForm.tsx
+++ front_end/src/components/StakeForm.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Token } from "./Main";
-import { useEthers, useTokenBalance } from "@usedapp/core";
-import { formatUnits } from "@ethersproject/units";
+import { useNotifications } from "@usedapp/core";
 import { Button, Input } from "@material-ui/core";
 import useStakeTokens from "../hooks/useStakeTokens";
 import { utils } from "ethers";
@@ -9,18 +8,34 @@ import { utils } from "ethers";
 const StakeForm: React.FC<{ token: Token }> = ({ token }) => {
   const [amount, setAmount] = React.useState<number>(0);
   const { address } = token;
-  const { approveTokenTransferAndStake, approveErc20State } =
-    useStakeTokens(address);
-  const { account } = useEthers();
-  const tokenBalance = useTokenBalance(address, account);
-  const formattedBalance: number = tokenBalance
-    ? parseFloat(formatUnits(tokenBalance, 18))
-    : 0;
+  const { approveTokenTransferAndStake } = useStakeTokens(address);
+  const { notifications } = useNotifications();
 
   const handleSubmit = () => {
     const amountToStakeInWei = utils.parseEther(amount.toString()).toString();
     approveTokenTransferAndStake(amountToStakeInWei);
   };
+
+  React.useEffect(() => {
+    if (
+      notifications.filter(
+        (notification) =>
+          notification.type === "transactionSucceed" &&
+          notification.transactionName === "Approve ERC20 transfer"
+      ).length > 0
+    ) {
+      console.log("approved!");
+    }
+    if (
+      notifications.filter(
+        (notification) =>
+          notification.type === "transactionSucceed" &&
+          notification.transactionName === "Stake tokens"
+      ).length > 0
+    ) {
+      console.log("staked!");
+    }
+  }, [notifications]);
 
   return (
     <>
