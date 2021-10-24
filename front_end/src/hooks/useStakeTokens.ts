7aac428536b8fd8ce1eed99c6daea554ec90bade
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 21:56:29 +0530

    update: updated the linting

--- front_end/src/hooks/useStakeTokens.ts
+++ front_end/src/hooks/useStakeTokens.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEthers, useContractFunction } from "@usedapp/core";
 import TokenFarm from "../chain-info/contracts/TokenFarm.json";
 import ERC20 from "../chain-info/contracts/dependencies/OpenZeppelin/openzeppelin-contracts@4.2.0/ERC20.json";
