b7a171ade92d73b485182118703836b2107526d4
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 18:33:07 +0530

    update: updated the chain toggling error handler

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -13,18 +13,22 @@ const Main: React.FC = () => {
   /**
    * getting the contract addresses for Dapp-token and Token-Farm. If chainId is not present then use a constant zero address(0x0000...) from ethers
    */
-  const dappTokenAddress = chainId
-    ? mapConfig[String(chainId) as "42"].DappToken[0]
-    : constants.AddressZero;
-  const wethTokenAddress = chainId
-    ? brownieConfig.networks[networkName as "kovan"].weth_token
-    : constants.AddressZero;
-  const fauTokenAddress = chainId
-    ? brownieConfig.networks[networkName as "kovan"].fau_token
-    : constants.AddressZero;
-  const tokenFarmAddress = chainId
-    ? mapConfig[String(chainId) as "42"].TokenFarm[0]
-    : constants.AddressZero;
+  const dappTokenAddress =
+    networkName === "kovan"
+      ? mapConfig[String(chainId) as "42"].DappToken[0]
+      : constants.AddressZero;
+  const wethTokenAddress =
+    networkName === "kovan"
+      ? brownieConfig.networks[networkName as "kovan"].weth_token
+      : constants.AddressZero;
+  const fauTokenAddress =
+    networkName === "kovan"
+      ? brownieConfig.networks[networkName as "kovan"].fau_token
+      : constants.AddressZero;
+  const tokenFarmAddress =
+    networkName === "kovan"
+      ? mapConfig[String(chainId) as "42"].TokenFarm[0]
+      : constants.AddressZero;
   return <></>;
 };
 
