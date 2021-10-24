0f402f81f7e68d4ce338828b6a7817810dc1b791
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 21:29:07 +0530

    add: added logic for getting the abi token-farm address and chainid in the useStakeTokens hook

--- front_end/src/hooks/useStakeTokens.ts
+++ front_end/src/hooks/useStakeTokens.ts
@@ -1,3 +1,17 @@
-const useStakeTokens = () => {};
+import { useEthers } from "@usedapp/core";
+import TokenFarm from "../chain-info/contracts/TokenFarm.json";
+import mapConfig from "../chain-info/deployments/map.json";
+import { constants } from "ethers";
+
+const useStakeTokens = (tokenAddress: string) => {
+  /**
+   * section for approving or checking approve token in token-farm
+   */
+  const { chainId } = useEthers();
+  const { abi } = TokenFarm;
+  const tokenFarmAddress = chainId
+    ? mapConfig[String(chainId) as "42"].TokenFarm[0]
+    : constants.AddressZero;
+};
 
 export default useStakeTokens;
