d261e07c5828e508d5cfe0502e17124327d0a82c
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-24 20:47:36 +0530

    add: added required content for balance-message

--- front_end/src/components/BalanceMsg.tsx
+++ front_end/src/components/BalanceMsg.tsx
@@ -1,4 +1,20 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "inline-grid",
+    gridTemplateColumns: "auto auto auto",
+    gap: theme.spacing(1),
+    alignItems: "center",
+  },
+  tokenImg: {
+    width: "32px",
+  },
+  amount: {
+    fontWeight: 700,
+  },
+}));
 
 interface BalanceMsgProps {
   label: string;
@@ -7,7 +23,14 @@ interface BalanceMsgProps {
 }
 
 const BalanceMsg: React.FC<BalanceMsgProps> = (props) => {
-  return <></>;
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <div>{props.label}</div>
+      <div className={classes.amount}>{props.amount}</div>
+      <img className={classes.tokenImg} src={props.tokenImg} alt="token-logo" />
+    </div>
+  );
 };
 
 export default BalanceMsg;
